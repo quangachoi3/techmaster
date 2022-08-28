3ff86964deb6ddcbb5648d9543cb217ab2384f65
quanpham <quancbq@gmail.com>
2022-08-28 18:31:14 +0700

    nop bai 3

--- JAVA-CORE/Lesson3/src/Main.java
+++ JAVA-CORE/Lesson3/src/Main.java
@@ -2,16 +2,47 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        String chuoi = "Pham Hong Quan";
-        Scanner sc = new Scanner(System.in);
-        int index = 0;
-        System.out.println(chuoi);
+//        String chuoi = "Pham Hong Quan";
+//        Scanner sc = new Scanner(System.in);
+//        int index = 0;
+//        System.out.println(chuoi);
+//
+//        System.out.println("Nhap chi muc");
+//        index = sc.nextInt();
+//
+//        System.out.println("Ky tu " + index + " la:" + chuoi.charAt(index));
 
-        System.out.println("Nhap chi muc");
-        index= sc.nextInt();
+        bai2();
+    }
 
-        System.out.println("Ky tu " + index + " la:" + chuoi.charAt(index));
-
-
+    public static void bai2() {
+        String str = "techmaster";
+        String result = "";
+        int length = str.length();
+        result += str.charAt(0);
+        for (int i = 1; i < length; i++) {
+            if (false == isExist(str.charAt(i), result))
+            {
+                result += str.charAt(i);
+            }
+        }
+        System.out.println(result);
+    }
+    public static boolean isExist(char ch, String str) {
+        int length = str.length();
+        boolean flag = false;
+        for (int i = 1; i < length; i++) {
+            if(str.charAt(i) == ch)
+            {
+                flag = true;
+                break;
+            }
+            else {
+                flag = false;
+            }
+        }
+        return flag;
     }
 }
+
+
