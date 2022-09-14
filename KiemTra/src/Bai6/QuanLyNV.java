4fe014ad8d26687c5de9203eedb0abb8125b4035
Quan <quancbq@gmail.com>
2022-09-14 20:20:50 +0700

    add bai tap 6

--- KiemTra/src/Bai6/QuanLyNV.java
+++ KiemTra/src/Bai6/QuanLyNV.java
@@ -2,25 +2,59 @@ package Bai6;
 
 
 
+import Bai5.Student;
+
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class QuanLyNV {
-    ArrayList<CBNV> CBNV_list = new ArrayList<>();
+    ArrayList<CBNV> CBNV_lists;
     private static QuanLyNV manager;
 
     public QuanLyNV() {
-
+        this.CBNV_lists = new ArrayList<CBNV>();
     }
 
     public static QuanLyNV getManager() {
         return new QuanLyNV();
     }
 
-    public static boolean addNV(CBNV nv){
-        return manager.CBNV_list.add(nv);
+    public void addNV(CBNV nv){
+        this.CBNV_lists.add(nv);
     }
-    public static boolean remove(){
-        int index = manager.CBNV_list.lastIndexOf()
-        return manager.CBNV_list.add(nv);
+    public void removeNV(String maGV){
+        ListIterator<CBNV> iterator = this.CBNV_lists.listIterator();
+        CBNV index;
+        while (iterator.hasNext()) {
+            index = iterator.next();
+            if(index.thongTinNV.getId().equals(maGV))
+            {
+                CBNV_lists.remove(index);
+            }
+        }
     }
+
+    public void showAll(){
+        ListIterator<CBNV> iterator = this.CBNV_lists.listIterator();
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next().toString());
+        }
+    }
+
+    public long tinhLuong(String maGV){
+        ListIterator<CBNV> iterator = this.CBNV_lists.listIterator();
+        CBNV index;
+        long result = 0;
+        while (iterator.hasNext()) {
+            index = iterator.next();
+            if(index.thongTinNV.getId().equals(maGV))
+            {
+                result = index.luongCung + index.thuong - index.tienPhat;
+                index.setLuongThucLinh(result);
+            }
+        }
+        return result;
+    }
+
+
 }
--- KiemTra/src/Main.java
+++ KiemTra/src/Main.java
@@ -1,16 +1,34 @@
 import Bai5.Student;
 import Bai5.StudentManagement;
+import Bai6.CBNV;
+import Bai6.Nguoi;
+import Bai6.QuanLyNV;
 
 public class Main {
     public static void main(String[] args) {
-        StudentManagement student_manager = StudentManagement.getStudentManager();
-        Student student1 = new Student("Quan", 19, "ThaiBinh");
-        Student student2 = new Student("Tho", 21, "ThanhHoa");
-        Student student3 = new Student("Tien", 19, "HaNoi");
-        student_manager.addStudent(student1);
-        student_manager.addStudent(student2);
-        student_manager.addStudent(student3);
+//        StudentManagement student_manager = StudentManagement.getStudentManager();
+//        Student student1 = new Student("Quan", 19, "ThaiBinh");
+//        Student student2 = new Student("Tho", 21, "ThanhHoa");
+//        Student student3 = new Student("Tien", 19, "HaNoi");
+//        student_manager.addStudent(student1);
+//        student_manager.addStudent(student2);
+//        student_manager.addStudent(student3);
 //        student_manager.showStudents();
-        student_manager.showStudentsUnder(20);
+//        student_manager.showStudentsUnder(20);
+
+        QuanLyNV nv_manager = QuanLyNV.getManager();
+        Nguoi nguoi1 = new Nguoi("Quan", 19, "ThaiBinh", "AAA");
+        Nguoi nguoi2 = new Nguoi("Tho", 21, "ThanhHoa", "AAB");
+        Nguoi nguoi3 = new Nguoi("Tien", 19, "HaNoi", "AAC");
+        CBNV nv1 = new CBNV(10, 10, 1, 10, nguoi1 );
+        CBNV nv2 = new CBNV(100, 100, 10, 100, nguoi2 );
+        CBNV nv3 = new CBNV(15, 15, 1, 15, nguoi3 );
+//        System.out.println(nv3.toString());
+        nv_manager.addNV(nv1);
+        nv_manager.addNV(nv2);
+        nv_manager.addNV(nv3);
+        nv_manager.removeNV("AAB");
+        nv_manager.tinhLuong("AAA");
+        nv_manager.showAll();
     }
 }
